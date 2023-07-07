83cd4faa316402a51af70d2fa928a6f797b8a6ad
UlianaMeashkova <el.mahno@gmail.com>
2023-07-07 22:01:43 +0300

    all tests good

--- sofaro/products/tests/test_views.py
+++ sofaro/products/tests/test_views.py
@@ -13,16 +13,100 @@ class UsersTest(TestCase):
 
         Hotels.objects.create(
             country="Турция",
-            title="Hotel 19"
+            title="Hotel 1"
         )
         Product.objects.create(
-            title="Product 19",
+            title="Product 1",
             country="Турция",
             price=4000,
             price_usd=1400
         )
         self.client = Client()
-        self.client.login(username="tester", password="1")
+        self.client.login(username="27062018aq@gmail.com", password="321675")
+
+
+    def test_index(self):
+
+        response = self.client.get(reverse("index"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_hotels(self):
+
+        response = self.client.get(reverse("hotels", kwargs={"country": "Турция"}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_booking_GET(self):
+
+        response = self.client.get(reverse("booking"))
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.context["form"], BookingForm)
+
+    def test_booking_POST(self):
+
+        response = self.client.post(reverse("booking"), {
+            "first_name": "Tester",
+            "last_name": "Tester",
+            "email": "27062018aq@gmail.com",
+            "password": "321675",
+            "age": 18
+        })
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_good_book(self):
+
+        response = self.client.get(reverse("goodBook"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_contacts(self):
+
+        response = self.client.get(reverse("contacts"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_users(self):
+
+        response = self.client.get(reverse("users"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content.decode(), "Profiles view")
+
+    def test_comment_GET(self):
+
+        response = self.client.get(reverse("comment", kwargs={"hotel_id": 1}))
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.context["form"], CommentForm)
+
+    def test_comment_POST(self):
+
+        response = self.client.post(reverse("comment", kwargs={"hotel_id": 1}), {
+            "name": "Tester",
+            "email": "test@test.com",
+            "body": "Test message...",
+        })
+        comment = Comment.objects.first()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(comment.id, 1)
+        self.assertEqual(comment.post.id, 1)
+        self.assertEqual(comment.body, "Test message...")
+
+    def test_score_GET(self):
+
+        response = self.client.get(reverse("score", kwargs={"hotel_id": 1}))
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.context["form"], ScoreForm)
+    
+    def test_score_POST(self):
+
+        product = Product.objects.get(id=1)
+        self.assertEqual(product.get_score(), 0)
+
+        response = self.client.post(reverse("score", kwargs={"hotel_id": 1}), {
+            "value": 5,
+        })
+        score = Score.objects.first()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(product.get_score(), 5.0)
+    
 
 
     
--- sofaro/sofaro/settings.py
+++ sofaro/sofaro/settings.py
@@ -150,7 +150,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 
